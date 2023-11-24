7a00464b5ef9c1b998aa8bb760be0e01f42e925d
gjelsoe <36234524+gjelsoe@users.noreply.github.com>
2023-11-24 13:40:13 +0100

    Update front_settings.sh
    Width adjusted for help text and some of the code never was written to repo.

--- front_settings.sh
+++ front_settings.sh
@@ -31,7 +31,7 @@ edit_settings() {
     done < "$SETTINGS_FILE"
 
     # Prompt user to select items to edit
-    SELECTED_ITEM=$(whiptail --title "Edit Settings" --menu "Select an item to edit:" 20 140 10 --cancel-button "Back" "${SETTINGS_ARRAY[@]}" 3>&1 1>&2 2>&3)
+    SELECTED_ITEM=$(whiptail --title "Edit Settings" --menu "Select an item to edit:" 20 155 10 --cancel-button "Back" "${SETTINGS_ARRAY[@]}" 3>&1 1>&2 2>&3)
 
     # Check if cancel button is pressed
     if [ $? -ne 0 ]; then
@@ -41,3 +41,74 @@ edit_settings() {
     OLD_VALUE=$(grep "^$SELECTED_ITEM=" "$SETTINGS_FILE" | cut -d "=" -f 2)
     NEW_VALUE=$(whiptail --title "Edit Settings" --inputbox "$SELECTED_ITEM - ${SETTINGS_ARRAY[1]}" 10 40 "$OLD_VALUE" 3>&1 1>&2 2>&3)
 
+    if [ $? -eq 0 ]; then
+      sed -i "s/^$SELECTED_ITEM=.*/$SELECTED_ITEM=$NEW_VALUE/" "$SETTINGS_FILE"
+      whiptail --title "Success" --msgbox "Settings updated successfully!" 10 40
+    else
+      whiptail --title "Warning" --msgbox "No changes made." 10 40
+    fi
+  done
+}
+
+# Function to generate a bash file for FrontServer
+generate_frontserver_bash_file() {
+  FILENAME=$(whiptail --title "Generate FrontServer Bash File" --inputbox "Enter the filename:" 10 40 3>&1 1>&2 2>&3)
+
+  if [ $? -eq 0 ]; then
+    SERVER_SETTINGS=()
+    SERVER_NAME=""
+    SERVER_PASSWORD=""
+    USE_STEAM_SOCKET=""
+    MAX_PLAYERS=""
+
+    while IFS="=" read -r ITEM VALUE; do
+      case "$ITEM" in
+        "ServerName") SERVER_NAME="$VALUE" ;;
+        "ServerPassword") SERVER_PASSWORD="$VALUE" ;;
+        "UseSteamSocket") USE_STEAM_SOCKET="$VALUE" ;;
+        "QueueThreshold") MAX_PLAYERS="$VALUE" ;;
+        *) SERVER_SETTINGS+=("-$ITEM=$VALUE") ;;
+      esac
+    done < "$SETTINGS_FILE"
+
+    # Determine the FRONT_SERVER value based on UseSteamSocket and USE_EPIC
+    if [ "$USE_STEAM_SOCKET" -eq 1 ]; then
+        if [ "$USE_EPIC" -eq 1 ]; then
+            FRONT_SERVER="./FrontServer ProjectWar_Start?DedicatedServer?MaxPlayers=$MAX_PLAYERS?udrs=eos"
+        else
+            FRONT_SERVER="./FrontServer ProjectWar_Start?DedicatedServer?MaxPlayers=$MAX_PLAYERS?udrs=steam"
+        fi
+    else
+        FRONT_SERVER="./FrontServer ProjectWar_Start?DedicatedServer?MaxPlayers=$MAX_PLAYERS"
+    fi
+
+    cat <<EOL >"$FILENAME.sh"
+
+#!/bin/bash
+#
+# Enters The Front Server Dir.
+cd $FRONT_SERVERDIR
+#
+# Execute FrontServer with arguments
+$FRONT_SERVER -ServerName="$SERVER_NAME" -ServerPassword="$SERVER_PASSWORD" -UserDir="$FRONT_USERDIR" ${SERVER_SETTINGS[@]}
+EOL
+
+    # Make the script executable
+    chmod +x "$FILENAME.sh"
+
+    whiptail --title "Success" --msgbox "Bash file '$FILENAME.sh' created successfully!" 10 40
+  else
+    whiptail --title "Warning" --msgbox "No file created." 10 40
+  fi
+}
+
+# Main loop
+while true; do
+  show_main_menu
+
+  case $CHOICE in
+    "1") edit_settings ;;
+    "2") generate_frontserver_bash_file ;;
+    "3") exit ;;
+  esac
+done
